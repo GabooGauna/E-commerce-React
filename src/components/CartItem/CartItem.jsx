77e9603737deb0b0f0cc0abfca547706069dc758
Gauna Cesar Gabriel <cesarggauna@gmail.com>
2022-06-10 06:20:55 -0300

    creacion de estilos Cart

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -5,10 +5,22 @@ const CartItem = ({ item }) => {
   const { deleteById } = useCartContext();
   console.log(item);
   return (
-    <div>
-      <h1 className="text-dark">{item.name}</h1>
-      <h4 className="text-dark">Unidades: {item.quantity}</h4>
-      <button onClick={() => deleteById(item.id)}>Eliminar</button>
+    <div
+      className="card m-auto mt-5 mb-5 p-0 shadow-lg col-4"
+      style={{ width: "18rem" }}
+    >
+      <img
+        className="card-img-top"
+        src={item.thumbnailUrl}
+        alt="Card image cap"
+      />
+      <div className="card-body card-bg ">
+        <h6 className="card-title text-center">{item.title}</h6>
+        <p className="card-text text-center p-carta">{item.quantity}</p>
+      </div>
+      <button className="boton" onClick={() => deleteById(item.id)}>
+        Eliminar
+      </button>
     </div>
   );
 };
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,23 +1,60 @@
 import React from "react";
+import "./Cart.css";
 import CartItem from "../../components/CartItem/CartItem";
 import { useCartContext } from "../../context/CartContextProvider";
+import {
+  collection,
+  addDoc,
+  getFirestore,
+  Timestamp,
+} from "firebase/firestore";
 
 function Cart() {
   const { cartList, emptyCart, totalPrice } = useCartContext();
-  console.log(cartList);
+  const finalizarCompra = async () => {
+    const db = getFirestore();
+    const docRef = await addDoc(collection(db, "orders"), {
+      buyer: {
+        name: "Francisco de los Santos",
+        phone: 3143324234,
+        emal: "franchu@hotmail.com",
+      },
+      items: cartList.map((item) => ({
+        title: item.title,
+        price: item.precio,
+        id: item.id,
+      })),
+      date: Timestamp.fromDate(new Date()),
+      total: totalPrice(),
+    });
+    alert("Tu orden fue enviada con éxito");
+  };
   return (
-    <div>
+    <div className="row cartcontainer">
       {cartList.map((item) => (
         <CartItem key={item.id} item={item} />
       ))}
       {cartList.length > 0 ? (
-        <>
-          <button onClick={() => emptyCart()}>Vaciar Carrito</button>
-          <h4>Precio total: ${totalPrice()}</h4>
-        </>
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <h4 className="precio-title">Precio total: ${totalPrice()}</h4>
+          <button
+            className="btn-primary btn-lg mt-3 mb-3"
+            onClick={() => emptyCart()}
+          >
+            Vaciar Carrito
+          </button>
+        </div>
       ) : (
         <h1>El Carrito está vacio</h1>
       )}
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <button
+          className="btn-primary btn-lg mt-3 mb-3"
+          onClick={finalizarCompra}
+        >
+          Finalizar Compra
+        </button>
+      </div>
     </div>
   );
 }
